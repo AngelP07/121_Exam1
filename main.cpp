c33ec7316cff45e4495786084cff088e8d602f8e
AngelP07 <166457586+AngelP07@users.noreply.github.com>
2025-03-26 21:23:39 -0700

    Option B, choices 1-3 completed

--- main.cpp
+++ main.cpp
@@ -3,12 +3,19 @@
 #include<iostream>
 #include<iomanip>
 #include<cctype>
+#include<fstream>
 #include "input.h"
 using namespace std;
 
 void advancedBinaryFile();
-//void dynamicArray();
+
+void dynamicArray();
+//void fileToArray(string filename, Element* array, int size);
+//void displayArray();
+
+
 //void vector();
+// 
 //void xtraCredit();
 
 
@@ -26,11 +33,12 @@ int main()
         //Space for Periodic Table
 
         cout << "\n\t" << char(201);
-        while (j < 160)
+        /*while (j < 160)
         {
             cout << char(205);
             j++;
-        }
+        }*/
+        cout << string(160, char(205));
         cout << char(187);
         cout << "\n\t" << char(186) << "                                       Periodic Table of Elements by Atomic Number & Symbol(colors 5 pts Xtra credits)                                          " << char(186);
         cout << "\n\t" << char(186) << "       [  1-H ]                                                                                                                                  [  2-He]       " << char(186);
@@ -38,19 +46,20 @@ int main()
         cout << "\n\t" << char(186) << "       [ 11-Na][ 12-Mg]                                                                                  [ 13-Al][ 14-Si][ 15-P ][ 16-S ][ 17-Cl][ 18-Ar]       " << char(186);
         cout << "\n\t" << char(186) << "       [ 19-K ][ 20-Ca][ 21-Sc]  [ 22-Ti][ 23-V ][ 24-Cr][ 25-Mn][ 26-Fe][ 27-Co][ 28-Ni][ 29-Cu][ 30-Zn][ 31-Ga][ 32-Ge][ 33-As][ 34-Se][ 35-Br][ 36-Kr]       " << char(186);
         cout << "\n\t" << char(186) << "       [ 37-Rb][ 38-Sr][ 39-Y ]  [ 40-Zr][ 41-Nb][ 42-Mo][ 43-Tc][ 44-Ru][ 45-Rh][ 46-Pd][ 47-Ag][ 48-Cd][ 49-In][ 50-Sn][ 51-Sb][ 52-Te][ 53-I ][ 54-Xe]       " << char(186);
-        cout << "\n\t" << char(186) << "       [ 55-Cs][ 56-Ba][ 57-La]"<< char(196) << char(191) <<"[ 72-Hf][ 73-Ta][ 74-W ][ 75-Re][ 76-Os][ 77-Ir][ 78-Pt][ 79-Au][ 80-Hg][ 81-Tl][ 82-Pb][ 83-Bi][ 84-Po][ 85-At][ 86-Rn]       " << char(186);
+        cout << "\n\t" << char(186) << "       [ 55-Cs][ 56-Ba][ 57-La]" << char(196) << char(191) << "[ 72-Hf][ 73-Ta][ 74-W ][ 75-Re][ 76-Os][ 77-Ir][ 78-Pt][ 79-Au][ 80-Hg][ 81-Tl][ 82-Pb][ 83-Bi][ 84-Po][ 85-At][ 86-Rn]       " << char(186);
         cout << "\n\t" << char(186) << "       [ 87-Fr][ 88-Ra][ 89-Ac]" << char(191) << char(179) << "[104-Rf][105-Db][106-Sg][107-Bh][108-Hs][109-Mt][110-Ds][111-Rg][112-Cn][113-Nh][114-Fl][115-Mc][116-Lv][117-Ts][118-Og]       " << char(186);
-        cout << "\n\t" << char(186) << "                               " << char(179) << char(179) <<"                                                                                                                               " << char(186);
+        cout << "\n\t" << char(186) << "                               " << char(179) << char(179) << "                                                                                                                               " << char(186);
         cout << "\n\t" << char(186) << "                               " << char(179) << char(192) << "[ 58-Ce][ 59-Pr][ 60-Nd][ 61-Pm][ 62-Sm][ 63-Eu][ 64-Gd][ 65-Tb][ 66-Dy][ 67-Ho][ 68-Er][ 69-Tm][ 70-Yb][ 71-Lu]               " << char(186);
         cout << "\n\t" << char(186) << "                               " << char(192) << "[ 90-Th][ 91-Pa][ 92-U ][ 93-Np][ 94-Pu][ 95-Am][ 96-Cm][ 97-Bk][ 98-Cf][ 99-Es][100-Fm][101-Md][102-No][103-Lr]                " << char(186);
         cout << "\n\t" << char(200);
-        
-        j = 0;
+
+        /*j = 0;
         while (j < 160)
         {
             cout << char(205);
             j++;
-        }
+        }*/
+        cout << string(160, char(205));
         cout << char(188);
 
         cout << "\n\n\t" << string(162, char(205));
@@ -64,17 +73,19 @@ int main()
         cout << "\n\t Q) Quit";
         cout << "\n\t" << string(162, char(205));
 
-        switch (inputInteger("\n\tOption: ", 0, 20))
+
+        //check how to use toUpper
+        switch (toupper(inputChar("\n\tOption: ", "ABCXQ")))
         {
         case 'Q': exit(0);
-        case 'A': advancedBinaryFile(); 
-        //  break;
-        //case 'B': dynamicArray(); 
-        //  break;
-        //case 'C': vector(); 
-        //  break;
-        //case 'X': xtraCredit(); 
-        //  break;
+        case 'A': advancedBinaryFile();
+            break;
+        case 'B': dynamicArray();
+            break;
+            //case 'C': vector(); 
+            //  break;
+            //case 'X': xtraCredit(); 
+            //  break;
         }
 
         cout << "\n\n";
@@ -87,15 +98,16 @@ int main()
 
 struct Element
 {
+    short atomicNum = 0;
     char symbol[3] = "un";
     char name[25] = "unk";
     float mass = 0.0;
     char state = 'x';
     short groupNum = 0;
     short periodNum = 0;
-    short blockType = 0;
-    float meltingPt = 0;
-    float boilPt = 0;
+    char blockType = 'u';
+    float meltingPt = 0.0;
+    float boilPt = 0.0;
     short discoveryYr = 0;
     char scientist[100] = "unk";
 };
@@ -104,7 +116,7 @@ struct Element
 
 void advancedBinaryFile()
 {
-    do 
+    do
     {
         system("cls");
         cout << "\n\n\t" << string(80, char(205));
@@ -118,21 +130,275 @@ void advancedBinaryFile()
         cout << "\n\t 0. Return to main menu";
         cout << "\n\t" << string(80, char(205));
 
-        switch (inputInteger("\n\tOption: ", 0, 20))
+        switch (inputInteger("\n\tOption: ", 0, 4))
         {
         case 0: main();
-        //case 1: readFile();
+            //case 1: readFile();
             break;
-        //case 2: addElem(); 
+            //case 2: addElem(); 
             break;
-        //case 3: updateElem(); 
+            //case 3: updateElem(); 
             break;
-        //case 4: searchElem(); 
+            //case 4: searchElem(); 
             break;
         }
 
         cout << "\n\n";
         system("pause");
-        
+
     } while (true);
 }
+
+
+
+//Begining of Angel's work on dynamically allocated arrays: Option B (Chapter 9)
+
+//precondition  : takes in Element ptr, and int& arguements
+//postcondition : returns a copy of original ptr array that is one element larger
+Element* arrayExpander(Element* array, int& size) {
+
+    //creates a temp array that is one element longer than arguement
+    Element* temp = new Element[size + 1];
+
+    //copies elements from array to temp
+    for (int i = 0; i < size; i++)
+        *(temp + i) = *(array + i);
+
+    //deletes original array
+    delete[] array;
+
+    //returns temp as newly expanded array
+    return temp;
+}
+
+
+//precondition  : takes in string, Element ptr, and int& arguements
+//postcondition : if file exists then returns nothing, otherwise reads the files contents and saves them onto an array
+Element* fileToArray(string filename, Element* array, int& size)
+{
+    fstream file;
+    //initialize starting index
+    int i = 0;
+
+    //opens file for reading
+    file.open(filename, ios::binary | ios::in);
+
+    //if file does not exist then creates error code
+    if (!file)
+    {
+        cout << "\n\tERROR: File not found. Returning to menu...";
+    }
+    else
+    {
+        //replaces basic ptr with array ptr with 1 element
+        array = arrayExpander(array, size);
+
+        //reads data in binary file until no more data is found
+        while (file.read(reinterpret_cast<char*>(&array[i]), sizeof(array[i]))) 
+        {
+            //increments size to keep up with index 
+            size++;
+
+            /*cout << "\n\tIndex[" << i << "]";
+            cout << "\n\tAtomic #: " << array[i].atomicNum;
+            cout << "\n\tSymbol: " << array[i].symbol;
+            cout << "\n\tName: " << array[i].name;
+            cout << "\n\tState of matter: " << array[i].state;
+            cout << "\n\tGroup #: " << array[i].groupNum;
+            cout << "\n\tPeriod #: " << array[i].periodNum;
+            cout << "\n\tBlock type: " << array[i].blockType;
+            cout << "\n\tMelting point: " << array[i].meltingPt;
+            cout << "\n\tBoiling Point: " << array[i].boilPt;
+            cout << "\n\tDiscovery year: " << array[i].discoveryYr;
+            cout << "\n\tDiscovered by: " << array[i].scientist;
+            cout << "\n\n";*/
+    
+            //expands array by one element
+            array = arrayExpander(array, size);
+
+            //move to next element
+            i++;
+        }
+    }
+    //closes binary file
+    file.close();
+
+    //returns new array
+    return array;
+}
+
+//precondition  : takes in Element ptr, and int arguements
+//postcondition : prints out data stored in ptr structure
+void displayArray(Element* array, int size)
+{
+    //if array ptr points to nothing then produces error code
+    if (array == nullptr)
+        cout << "\n\tError: Dynamic array is empty. No info can be displayed.";
+    else
+        for (int i = 0; i < size; i++)
+        {
+            cout << "\n\tIndex[" << i << "]";
+            cout << "\n\tAtomic #        : " << array[i].atomicNum;
+            cout << "\n\tSymbol          : " << array[i].symbol;
+            cout << "\n\tName            : " << array[i].name;
+            cout << "\n\tMass            : " << array[i].mass << "u";
+            cout << "\n\tState of matter : " << array[i].state;
+            cout << "\n\tGroup #         : " << array[i].groupNum;
+            cout << "\n\tPeriod #        : " << array[i].periodNum;
+            cout << "\n\tBlock type      : " << array[i].blockType;
+            cout << "\n\tMelting point   : " << array[i].meltingPt;
+            cout << "\n\tBoiling Point   : " << array[i].boilPt;
+            cout << "\n\tDiscovery year  : " << array[i].discoveryYr;
+            cout << "\n\tDiscovered by   : " << array[i].scientist;
+            cout << "\n\n";
+        }
+
+    cout << "\n\t*Current size of the array: " << size;
+}
+
+//precondition  : takes in Element ptr and int& as arguments
+//postcondition : appends array with new element's info
+Element* addElem(Element* array, int& size)
+{
+    //expands array by one element
+    array = arrayExpander(array, size);
+    size++;
+
+    system("cls");
+
+    cout << "\n\tEnter new Element info for Index[" << size - 1 << "]";
+    cout << "\n\t" << string(80, char(205));
+
+    //asks for new element's atomic number
+    array[size - 1].atomicNum = inputInteger("\n\tAtomic # :", 1, 118);
+
+    //asks for new element's symbol
+    strncpy_s(array[size - 1].symbol, inputString("\n\tSymbol :",false).c_str(), sizeof(array[size - 1].symbol) - 1);
+
+    //asks for new element's name
+    strncpy_s(array[size - 1].name, inputString("\n\tName :", false).c_str(), sizeof(array[size - 1].name) - 1);
+
+    //asks for new element's mass
+    array[size - 1].mass = inputDouble("\n\tMass: ", true);
+
+    //asks for new element's state of matter
+    array[size - 1].state = toupper(inputChar("\n\tState of matter: "));
+
+    //asks for new element's group number
+    array[size - 1].groupNum = inputInteger("\n\tGroup # :", true);
+
+    //period number
+    array[size - 1].periodNum = inputInteger("\n\tPeriod # :", true);
+
+    //asks for new element's block type
+    array[size - 1].blockType = toupper(inputChar("\n\tBlock Type: "));
+
+    //asks for new element's melting point
+    array[size - 1].meltingPt = inputDouble("\n\tMelting point: ");
+    
+    //asks for new element's boiling point
+    array[size - 1].boilPt = inputDouble("\n\tBoiling point: ");
+
+
+    //asks for new element's year of discovery
+    array[size - 1].discoveryYr = inputInteger("\n\tDiscovery year: ", true);
+
+    //asks who discovered the element
+    strncpy_s(array[size - 1].scientist, inputString("\n\tDiscovered by:", false).c_str(), sizeof(array[size - 1].scientist) - 1);
+
+    //returns newly appended array
+    return array;
+}
+
+
+void dynamicArray()
+{
+    int numElements = 0;
+    Element* array = nullptr;
+    string binaryFileName;
+
+    do
+    {
+        system("cls");
+        cout << "\n\n\t" << string(80, char(205));
+        cout << "\n\tOption: Dynamic Array Menu";
+        cout << "\n\t" << string(80, char(205));
+        cout << "\n\t 1. Read element(s) from the binary data file and store into the dynamic array";
+        cout << "\n\t 2. Display element(s) from the dynamic array";
+        cout << "\n\t 3. Add a new chemistry element into the dynamic array";
+        cout << "\n\t 4. Update an existing chemistry element from the dynamic array";
+        cout << "\n\t 5. Sort the dynamic array by Symbol in descending order";
+        cout << "\n\t 6. Binary search an element by Symbol";
+        cout << "\n\t 7. Write elements from the dynamic array to the binary data file.";
+        cout << "\n\t" << string(80, char(196));
+        cout << "\n\t 0. Return to main menu";
+        cout << "\n\t" << string(80, char(205));
+
+        switch (inputInteger("\n\tOption: ", 0, 7))
+        {
+        case 0: main();
+            break;
+
+        case 1:
+            system("cls");
+            cout << "\n\tOption 1B: Read file and store array info";
+            cout << "\n\t" << string(80, char(205));
+            binaryFileName = inputString("\n\tEnter the binary file to be read from: ", false);
+            array = fileToArray(binaryFileName, array, numElements);
+            break;
+
+        case 2:
+            system("cls");
+            cout << "\n\tOption 2B: Display array info";
+            cout << "\n\t" << string(80, char(205));
+            displayArray(array, numElements);
+            break;
+        case 3: 
+            array = addElem(array, numElements); 
+            break;
+            //case 4: updateElem(); 
+            break;
+            //case 5: sortArr(); 
+            break;
+            //case 6: binarySearch(); 
+            break;
+            //case 7: writeToFile(); 
+            break;
+        }
+
+        cout << "\n\n";
+        system("pause");
+
+    } while (true);
+}
+
+//void displayArray(Element* array, int size)
+//{
+//    for (int i = 0; i < size; i++)
+//    {
+//        cout << "\n\tIndex[" << i << "]";
+//        cout << "\n\tAtomic #: " << array[i].atomicNum;
+//        cout << "\n\tSymbol: " << array[i].symbol;
+//        cout << "\n\tName: " << array[i].name;
+//        cout << "\n\tState of matter: " << array[i].state;
+//        cout << "\n\tGroup #: " << array[i].groupNum;
+//        cout << "\n\tPeriod #: " << array[i].periodNum;
+//        cout << "\n\tBlock type: " << array[i].blockType;
+//        cout << "\n\Melting point: " << array[i].meltingPt;
+//        cout << "\n\tBoiling Point: " << array[i].boilPt;
+//        cout << "\n\tDiscovery year: " << array[i].discoveryYr;
+//        cout << "\n\tDiscovered by: " << array[i].scientist;
+//    }
+//}
+
+//void fileToArray(string filename, Element* array, int size)
+//{
+//    fstream file;
+//
+//    file.open(filename, ios::binary | ios::in);
+//
+//    for (int i = 0; i < size; i++) {
+//        file.read(reinterpret_cast<char*>(&array[i]), sizeof(array[i]));
+//    }
+//    file.close();
+//}
