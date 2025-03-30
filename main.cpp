19ee66a70c355fa7a6bf8b2a2705e8dee52b87dc
AngelP07 <166457586+AngelP07@users.noreply.github.com>
2025-03-30 11:12:40 -0700

    Update<OptionB> main.cpp
    Completed Option B *NO INPUT VALIDATION, ONLY FUNCTIONS COMPLETED*

--- main.cpp
+++ main.cpp
@@ -31,6 +31,7 @@ int main()
 
 
         //Space for Periodic Table
+
         cout << "\n\t" << char(201);
         cout << string(160, char(205));
         cout << char(187);
@@ -164,6 +165,7 @@ Element* arrayExpander(Element* array, int& size) {
 Element* fileToArray(string filename, Element* array, int& size)
 {
     fstream file;
+    array = nullptr;
     //initialize starting index
     int i = 0;
 
@@ -185,7 +187,7 @@ Element* fileToArray(string filename, Element* array, int& size)
         {
             //increments size to keep up with index 
             size++;
-            
+
             //expands array by one element
             array = arrayExpander(array, size);
 
@@ -233,11 +235,14 @@ void displayArray(Element* array, int size)
 //postcondition : appends array with new element's info
 Element* addElem(Element* array, int& size)
 {
+    system("cls");
+
+    if (array == nullptr)
+            cout << "No array was found. Creating new array...";
+
     //expands array by one element
     array = arrayExpander(array, size);
     size++;
-
-    system("cls");
 
     cout << "\n\tEnter new Element info for Index[" << size - 1 << "]";
     cout << "\n\t" << string(80, char(205));
@@ -283,10 +288,258 @@ Element* addElem(Element* array, int& size)
     return array;
 }
 
+//precondition  : takes in Element ptr and short as arguements
+//postcondition : returns true if atomic number is found in the array, otherwise returns false
+bool checkAtomNum(Element* array, short atomicNum)
+{
+    bool state = false;
+
+    for(int i = 0; i < sizeof(array) + 1; i++)
+    {
+        if (atomicNum == array[i].atomicNum)
+        {
+            state = true;
+            break;
+        }
+
+    }
+
+    return state;
+}
+
+//precondition  : takes in Element ptr and int& as arguments
+//postcondition : replace index chosen with new element's info
+Element* updateElem(Element* array, int& size)
+{
+    short atomNum = 0;
+    short index = 0;
+    //create a temp array to hold edited info
+    Element* temp = new Element[size];
+
+    //copies array to temp 
+    for(int i = 0; i < size; i++)
+        *(temp + i) = *(array + i);
+
+    //if array ptr points to nothing then produces error code and returns
+    if (array == nullptr)
+    {
+        cout << "\n\tError: Dynamic array is empty. No info can be updated. Please select a valid option.";
+        return array;
+    }
+    else
+    {
+    
+        atomNum = inputInteger("\n\tChoose an atomic # of an existing Element: ", true);
+
+        //if atomic # is not found in array then returns the original array to previous menu
+        if (!checkAtomNum(array, atomNum))
+        {
+            cout << "\n\tERROR: Cannot update an element with Atomic # (" << atomNum << ") that does not existed.Please perform other valid option.";
+            return array;
+        }
+        //if atomic number is found in the array then then block continues
+        else 
+        {
+            //gets index where atomic number is held
+            for (int i = 0; i < sizeof(array) + 1; i++)
+            {
+                if (atomNum == array[i].atomicNum)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            do
+            {
+                system("cls");
+
+                //Shows the element info and updates info if it is changed for user to see
+                cout << "\n\tIndex[" << index << "]";
+                cout << "\n\tAtomic #        : " << temp[index].atomicNum;
+                cout << "\n\tSymbol          : " << temp[index].symbol;
+                cout << "\n\tName            : " << temp[index].name;
+                cout << "\n\tMass            : " << temp[index].mass << "u";
+                cout << "\n\tState of matter : " << temp[index].state;
+                cout << "\n\tGroup #         : " << temp[index].groupNum;
+                cout << "\n\tPeriod #        : " << temp[index].periodNum;
+                cout << "\n\tBlock type      : " << temp[index].blockType;
+                cout << "\n\tMelting point   : " << temp[index].meltingPt;
+                cout << "\n\tBoiling Point   : " << temp[index].boilPt;
+                cout << "\n\tDiscovery year  : " << temp[index].discoveryYr;
+                cout << "\n\tDiscovered by   : " << temp[index].scientist;
+                cout << "\n\nUpdating Element's info...";
+                cout << "\n\n\t" << string(80, char(205));
+                cout << "\n\t 1.  Symbol";
+                cout << "\n\t 2.  Name";
+                cout << "\n\t 3.  Atomic Mass";
+                cout << "\n\t 4.  State type";
+                cout << "\n\t 5.  Group #";
+                cout << "\n\t 6.  Block Type";
+                cout << "\n\t 7.  Period Type";
+                cout << "\n\t 8.  Melting Point";
+                cout << "\n\t 9.  Boiling Point";
+                cout << "\n\t 10. Discovery Year";
+                cout << "\n\t 11. Discovered By";
+                cout << "\n\t" << string(80, char(196));
+                cout << "\n\t-1. Return WITHOUT change(s)";
+                cout << "\n\t 0. Return WITH committed change(s)";
+                cout << "\n\t" << string(80, char(205));
+
+                //switch statement to edit specific info or to return
+                switch (inputInteger("\n\tOption: ", -1, 11))
+                {
+                //returns array with original info of element
+                case -1:
+                    cout << "\n\tWARNING: Returning WITHOUT change(s)...";
+                    return array;
+                    break;
+                //returns array with edited info of element
+                case 0:
+                    cout << "\n\tWARNING: Returning WITH change(s)...";
+                    return temp;
+                    break;
+                case 1:
+                    //asks for new element's symbol
+                    strncpy_s(temp[index].symbol, inputString("\n\tSymbol :", false).c_str(), sizeof(temp[index].symbol) - 1);
+                    break;
+                case 2:
+                    //asks for new element's name
+                    strncpy_s(temp[index].name, inputString("\n\tName :", false).c_str(), sizeof(temp[index].name) - 1);
+                    break;
+                case 3:
+                    //asks for new element's mass
+                    temp[index].mass = inputDouble("\n\tMass: ", true);
+                    break;
+                case 4:
+                    //asks for new element's state of matter
+                    temp[index].state = toupper(inputChar("\n\tState of matter: "));
+                    break;
+                case 5:
+                    //asks for new element's group number
+                    temp[index].groupNum = inputInteger("\n\tGroup # :", true);
+                    break;
+                case 6:
+                    //period number
+                    temp[index].periodNum = inputInteger("\n\tPeriod # :", true);
+                    break;
+                case 7:
+                    //asks for new element's block type
+                    temp[index].blockType = toupper(inputChar("\n\tBlock Type: "));
+                    break;
+                case 8:
+                    //asks for new element's melting point
+                    temp[index].meltingPt = inputDouble("\n\tMelting point: ");
+                    break;
+                case 9:
+                    //asks for new element's boiling point
+                    temp[index].boilPt = inputDouble("\n\tBoiling point: ");
+                    break;
+                case 10:
+                    //asks for new element's year of discovery
+                    temp[index].discoveryYr = inputInteger("\n\tDiscovery year: ", true);
+                    break;
+                case 11:
+                    //asks who discovered the element
+                    strncpy_s(temp[index].scientist, inputString("\n\tDiscovered by:", false).c_str(), sizeof(temp[index].scientist) - 1);
+                    break;
+                }
+
+                cout << "\n\n";
+                system("pause");
+
+            } while(true);
+        }
+    }
+}
+
+//precondition  : takes in two Element objects
+//postcondition : compares the symbols of each element and returns true if the first symbol is less(alphabeticaly) than the other
+bool compareSymbol(Element element1,Element element2) 
+{
+    //convert the c_strings to strings to be able to use logical arguement: >
+    if (string(element1.symbol) < string(element2.symbol))
+        return true;
+    else
+        return false;
+}
+
+//precondition  : takes in Element ptr and int& as arguments
+//postcondition : sorts the array by symbol in acscending order
+Element* arrSort(Element* array, int& size)
+{
+    if (array == nullptr)
+        cout << "\n\tError: Dynamic array is empty. No info can be displayed.";
+    else
+    {
+        //sort function
+        sort(array, array + size, compareSymbol);
+        cout << "\n\tArray has been sorted in descending order";
+    }
+    return array;
+}
+
+//precondition  : takes in Element, int, and c_string as arguments
+//postcondition : searches the sorted array
+int binarySearch(Element* array, int size, char* symbol)
+{
+    int left = 0;
+    int right = size - 1;
+
+    
+    if (array == nullptr)
+        return -2;
+    else
+    {
+        // Loop to implement Binary Search 
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int cmp = strcmp(array[mid].symbol, symbol);
+
+            if (cmp == 0) {
+                return mid; // Found
+            }
+            else if (cmp < 0) {
+                left = mid + 1; // Target is in the left half
+            }
+            else {
+                right = mid - 1; // Target is in the right half
+            }
+        }
+
+        return -1;
+    }
+}
+
+//precondition  : takes in string, Element ptr, and int& arguements
+//postcondition : if array exists then writes info onto a new file
+void arrayToFile(string filename, Element* array, int& size)
+{
+	fstream file;
+    //index to begin at start of array
+    int i = 0;
+
+    //opens file in out to write data onto file
+	file.open(filename, ios::binary | ios::out);
+	
+    //runs through elements of array and writes info onto data file
+     while (i < size)
+     {
+         file.write(reinterpret_cast<char*>(&array[i]), sizeof(array[i]));
+         i++;
+     }
+
+	file.close();
+
+    //Confirmation code
+    cout << "\n\tCONFRIMATION: array elements have been written onto file: " << filename <<".";
+}
 
 void dynamicArray()
 {
     int numElements = 0;
+    int result = 0;
+    char sym[3] = "un";
     Element* array = nullptr;
     string binaryFileName;
 
@@ -300,7 +553,7 @@ void dynamicArray()
         cout << "\n\t 2. Display element(s) from the dynamic array";
         cout << "\n\t 3. Add a new chemistry element into the dynamic array";
         cout << "\n\t 4. Update an existing chemistry element from the dynamic array";
-        cout << "\n\t 5. Sort the dynamic array by Symbol in descending order";
+        cout << "\n\t 5. Sort the dynamic array by Symbol in ascending order";
         cout << "\n\t 6. Binary search an element by Symbol";
         cout << "\n\t 7. Write elements from the dynamic array to the binary data file.";
         cout << "\n\t" << string(80, char(196));
@@ -327,15 +580,43 @@ void dynamicArray()
             displayArray(array, numElements);
             break;
         case 3: 
+            system("cls");
+            cout << "\n\tOption 3B: Add a new Chemical Element";
+            cout << "\n\t" << string(80, char(205));
             array = addElem(array, numElements); 
             break;
-            //case 4: updateElem(); 
+        case 4: 
+            system("cls");
+            cout << "\n\tOption 4B: Update a Chemical Element's Info";
+            cout << "\n\t" << string(80, char(205));
+            array = updateElem(array, numElements);
             break;
-            //case 5: sortArr(); 
+        case 5: 
+            system("cls");
+            cout << "\n\tOption 5B: Sort array";
+            cout << "\n\t" << string(80, char(205)); 
+            array = arrSort(array, numElements);
             break;
-            //case 6: binarySearch(); 
+        case 6:
+            system("cls");
+            cout << "\n\tOption 5B: Sort array";
+            cout << "\n\t" << string(80, char(205));
+            strncpy_s(sym, inputString("\n\tSymbol :", false).c_str(), sizeof(sym) - 1);
+            result = binarySearch(array, numElements, sym);
+            if (result == -2)
+                cout << "\n\tError: Dynamic array is empty. No info can be displayed.";
+            else if (result == -1)
+                cout << "\n\tElement (" << sym << ") not present in array";
+            else
+                cout << "\n\tElement (" << sym <<  ") found at index " << result << ".";
             break;
-            //case 7: writeToFile(); 
+        case 7: 
+            system("cls");
+            cout << "\n\tOption 1B: Read file and store array info";
+            cout << "\n\t" << string(80, char(205));
+            binaryFileName = inputString("\n\tEnter the binary file to write array info into: ", false);
+            arrayToFile(binaryFileName, array, numElements);
+            main();
             break;
         }
 
@@ -344,34 +625,3 @@ void dynamicArray()
 
     } while (true);
 }
-
-//void displayArray(Element* array, int size)
-//{
-//    for (int i = 0; i < size; i++)
-//    {
-//        cout << "\n\tIndex[" << i << "]";
-//        cout << "\n\tAtomic #: " << array[i].atomicNum;
-//        cout << "\n\tSymbol: " << array[i].symbol;
-//        cout << "\n\tName: " << array[i].name;
-//        cout << "\n\tState of matter: " << array[i].state;
-//        cout << "\n\tGroup #: " << array[i].groupNum;
-//        cout << "\n\tPeriod #: " << array[i].periodNum;
-//        cout << "\n\tBlock type: " << array[i].blockType;
-//        cout << "\n\Melting point: " << array[i].meltingPt;
-//        cout << "\n\tBoiling Point: " << array[i].boilPt;
-//        cout << "\n\tDiscovery year: " << array[i].discoveryYr;
-//        cout << "\n\tDiscovered by: " << array[i].scientist;
-//    }
-//}
-
-//void fileToArray(string filename, Element* array, int size)
-//{
-//    fstream file;
-//
-//    file.open(filename, ios::binary | ios::in);
-//
-//    for (int i = 0; i < size; i++) {
-//        file.read(reinterpret_cast<char*>(&array[i]), sizeof(array[i]));
-//    }
-//    file.close();
-//}
